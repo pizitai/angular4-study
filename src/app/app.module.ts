af95c8eb9d639484375ce1d1fb115b401ae1a1a2
wuzhangpeng <wzhangpeng1989@163.com>
2017-11-21 20:05:24 +0800

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { ProductComponent } from './product/product.component';
-import { StarsComponent } from './stars/stars.component';
-import { SearchComponent } from './search/search.component';
-import { HomeComponent } from './home/home.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { WuComponent } from './wu/wu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    CarouselComponent,
-    ProductComponent,
-    StarsComponent,
-    SearchComponent,
-    HomeComponent,
-    ProductDetailComponent,
-    WuComponent
+    AppComponent   
   ],
   imports: [
     BrowserModule,
