0e9bc85241eb11e2a00a5aeb18dc0f19a34e423a
wuzhangpeng <wzhangpeng1989@163.com>
2017-11-22 10:26:56 +0800

    resolve bug

--- src/app/product2/product2.component.ts
+++ src/app/product2/product2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Product, ProductService } from "../shared/product.service";
+import { ProductService, Product } from '../shared/product.service';
 import { AnotherProductService } from "../shared/another-product.service";
 
 @Component({
@@ -12,12 +12,12 @@ import { AnotherProductService } from "../shared/another-product.service";
 })
 export class Product2Component implements OnInit {
 
-   product:Product;
+  product: Product;
 
-  constructor(private productService:ProductService) { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.product=this.productService.getProduct()
+    this.product = this.productService.getProduct()
   }
 
 
