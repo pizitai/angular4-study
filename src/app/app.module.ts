c4682460e9930bba21ba0c91c7fbc2ca14c54de5
wuzhangpeng <wzhangpeng1989@163.com>
2017-11-23 14:50:05 +0800

    concet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpModule } from '.4.4.6@@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,11 +11,14 @@ import { BindComponent } from './bind/bind.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BindComponent,   
+    BindComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/bind/bind.component.ts
+++ src/app/bind/bind.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs";
+import { FormControl } from "@angular/forms";
+// import 'rxjs/Rx';
 
 @Component({
   selector: 'app-bind',
@@ -7,9 +9,14 @@ import { Observable } from "rxjs";
   styleUrls: ['./bind.component.scss']
 })
 export class BindComponent implements OnInit {
+  getStockInfo(value: string): void {
+    console.log(value)
+  }
   ngOnInit(): void {
     
   }
+
+  searchInput:FormControl = new FormControl();
 
   constructor() {
     Observable.from([1,2,3,4])
@@ -20,5 +27,12 @@ export class BindComponent implements OnInit {
         err => console.error(err),
         () => console.log('line is over')
       )
+    this.searchInput.valueChanges
+      .debounceTime(500)
+      .subscribe( stockCode => this.getStockInfo(stockCode))
+  }
+  
+  onKey(value:string){
+    console.log(value);
   }
 }
