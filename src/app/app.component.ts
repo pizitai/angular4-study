2d73a4e38918d7fd8aef1f3c8d2e0a479a1ea215
wuzhangpeng <wzhangpeng1989@163.com>
2017-11-28 09:53:24 +0800

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import {ChildComponent} from './child/child.component';
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { ChildComponent } from './child/child.component';
+import { Component, ViewChild, OnInit, AfterViewChecked, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit,
+  AfterViewChecked {
+  ngAfterViewChecked(): void {
+    console.log('父组件的视图变更检测完毕');
+  }
+  ngAfterViewInit(): void {
+    console.log('父组件的视图初始化完毕');
+    setTimeout(() => {
+      this.message = 'hello';
+    }, 0);
+  }
   ngOnInit(): void {
-    this.child1.greeting('Tom');
+    setInterval(() => {
+      this.child1.greeting('Tom');
+    }, 5000);
   }
   @ViewChild('child1')
-  child1:ChildComponent;
+  child1: ChildComponent;
   greeting: string = 'Hello';
+  message: string;
 
 
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit , AfterViewInit,
+  AfterViewChecked {
+  ngAfterViewChecked(): void {
+    console.log('子组件的视图变更检测完毕');
+  }
+  ngAfterViewInit(): void {
+    console.log('子组件的视图初始化完毕');
+  }
 
   constructor() { }
 
